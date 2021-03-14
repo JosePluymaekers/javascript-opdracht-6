8272c6d7e4f499f516503a959926fdf90de3e333
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-03-14 13:50:35 +0100

    Add files via upload

---
+++ JavascriptOpdracht6/script.js
@@ -0,0 +1,23 @@
+
+const makePizza = function () {
+    console.log("rol deel uit tot 3 mm")
+    console.log("bestrijk met pastasaus")
+    console.log("beleg met paprika, ham en ananas")
+    console.log("strooi kaas er over. Lekker veel")
+    console.log("doe in 15 minuten in voorverwarmde oven")
+}
+
+const makeSushi = function () {
+    console.log("kook de rijst")
+    console.log("neem een vel zeewier en leg een laagje rijst op")
+    console.log("leg de vulling erop")
+    console.log("rol de plak strak op")
+    console.log("snij de rol in stukjes")
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
+
